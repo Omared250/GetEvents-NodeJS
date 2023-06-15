d31ab8cd2690bcf2d3f37f0883f0df1991a3b871
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-06-15 11:59:34 +0200

    adding call back function to handle call to database

---
+++ controller/getEvents.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+// const Event = require('../models/Event');
+
+const getEvents = async( req, res = response ) => {
+
+    const events = await Event.find().populate('user', 'name');
+
+    res.json({
+        ok: true,
+        events
+    })
+
+};
