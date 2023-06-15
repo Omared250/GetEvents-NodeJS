7c2e3be2027a04908d860b01c6651692b4c2a90d
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-06-15 11:56:21 +0200

    adding index structure

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+
+// create express server
+const app = express();
+
+// DataBase
+dbConnection();
+
+// Public Directory
+app.use( express.static('public') );
+
+// read and parse body
+app.use( express.json() );
+
+// routes
+app.use('/api/events', require('./routes/events'));
+
+
+// Listening
+app.listen( process.env.PORT, () => {
+    console.log(`Server running on port ${ process.env.PORT }`);
+} )
