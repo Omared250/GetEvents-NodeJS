e3c18abafdd92084ddaaf3abbeb0cb1c078fb11b
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-06-15 11:57:16 +0200

    adding route basis

---
+++ routes/events.js
@@ -0,0 +1,12 @@
+/*
+    Event Routes
+    /api/events
+*/
+
+const { Router } = require('express');
+// const { deleteEvent } = require('../controller/deleteEvent');
+const router = Router();
+
+router.delete('/delete', '?' )
+
+module.exports = router;
